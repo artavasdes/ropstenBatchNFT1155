136855d1e496b8f35d4683127919e2397c78023f
artavasdes <vartanyildiz@gmail.com>
2022-02-02 18:05:38 -0500

    added polygon net

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,26 +4,41 @@ require("@nomiclabs/hardhat-ethers");
 
 const { API_URL, PRIVATE_KEY } = process.env;
 
+//Ropsten Network
+/*
 module.exports = {
-
    solidity: "0.8.0",
-
    defaultNetwork: "ropsten",
-
    networks: {
-
       hardhat: {},
-
       ropsten: {
-
          url: API_URL,
-
          accounts: [`0x${PRIVATE_KEY}`]
-
       }
-
    },
-
 }
+*/
+
+//Mumbai(Polygon testnet)
+module.exports = {
+   defaultNetwork: "matic",
+   networks: {
+     hardhat: {
+     },
+     matic: {
+       url: "API_URL",
+       accounts: [process.env.PRIVATE_KEY]
+     }
+   },
+   solidity: {
+     version: "0.8.0",
+     settings: {
+       optimizer: {
+         enabled: true,
+         runs: 200
+       }
+     }
+   },
+ }
 
 
